766880d5600517ee5db590ffa880c33fd219849f
Vitchyr Pong <vitchyr@gmail.com>
2019-04-30 19:51:24 -0700

    remove cxx dependency

--- Formula/inkscape.rb
+++ Formula/inkscape.rb
@@ -46,7 +46,7 @@ class Inkscape < Formula
   depends_on "gdl" if build.with? "gtk3"
   depends_on "gtkmm" if build.without? "gtk3"
 
-  needs :cxx11
+  #needs :cxx11
 
   if MacOS.version < :mavericks
     fails_with :clang do
